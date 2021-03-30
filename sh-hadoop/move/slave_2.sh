d5ad2fb84edfa01d0d4de8579b03454c11acfccf
JhonnyAmplitude <62213604+JhonnyAmplitude@users.noreply.github.com>
2021-03-30 14:50:19 +0200

    Delete slave_2.sh

--- sh-hadoop/move/slave_2.sh
+++
@@ -1,6 +0,0 @@
-sudo gedit masters #Указываем в файле имя мастера
-sudo rm -rf /usr/local/hadoop/hadoop_tmp/
-sudo mkdir -p /usr/local/hadoop/hadoop_tmp/hdfs/datanode
-sudo chown hduser:hadoop -R /usr/local/hadoop/hadoop_tmp/
-/usr/local/hadoop/hadoop-2.10.1/sbin/start-dfs.sh
-/usr/local/hadoop/hadoop-2.10.1/sbin/start-yarn.sh
